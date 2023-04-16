0be3603038a24022464fee987dce5b6e17114971
mwyzer <muhammad.wyzer@gmail.com>
2023-04-17 04:33:38 +0700

    update product

---
+++ backend/app/Controllers/Products.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\ProductModel;
+
+class Products extends ResourceController
+{
+    /**
+     * Return an array of resource objects, themselves in array format
+     *
+     * @return mixed
+     */
+    use ResponseTrait;
+    public function index()
+    {
+        $model = new ProductModel();
+        $data = $model->findAll();
+        return $this->respond($data);
+    }
+
+    /**
+     * Return the properties of a resource object
+     *
+     * @return mixed
+     */
+    public function show($id = null)
+    {
+        $model = new ProductModel();
+        $data = $model->find(['id' => $id]);
+        if(!$data) return $this->FailNotFound('Data not found');
+        return $this->respond($data[0]);
+    }
+
+    /**
+     * Create a new resource object from "posted" parameters
+     *
+     * @return mixed
+     */
+    public function create()
+    {
+        helper(['form']);
+        $rules = [
+            'title' => 'required',
+            'price' => 'required'
+        ];
+        $data = [
+            'title' => $this->request->getVar('title'),
+            'price' => $this->request->getVar('price')
+        ];
+        if(!$this->validate($rules)) return $this->fail($this->validator->getErrors());
+        $model = new ProductModel();
+        $model->save($data);
+        $response = [
+            'status' => 201,
+            'error' => null,
+            'messages' => [
+                'success' => 'Success data created' 
+            ]
+        ];
+        return $this->respondCreated($response);
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties
+     *
+     * @return mixed
+     */
+    public function update($id = null)
+    {
+        helper(['form']);
+        $rules = [
+            'title' => 'required',
+            'price' => 'required'
+        ];
+        $data = [
+            'title' => $this->request->getVar('title'),
+            'price' => $this->request->getVar('price')
+        ];
+        if(!$this->validate($rules)) return $this->fail($this->validator->getErrors());
+        $model = new ProductModel();
+        $findById = $model->find(['id' => $id]);
+        if(!$findById) return $this->FailNotFound('Data not found');
+        $model-> update($id, $data);
+        $response = [
+            'status' => 200,
+            'error' => null,
+            'messages' => [
+                'success' => 'data updated success' 
+            ]
+        ];
+        return $this->respond($response);
+    }
+
+    /**
+     * Delete the designated resource object from the model
+     *
+     * @return mixed
+     */
+    public function delete($id = null)
+    {
+        //
+    }
+}
